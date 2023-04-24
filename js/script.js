105531d5857fc89fa54c102b64dc1eb6a4199999
hyeonglet <109091684+hyeonglet@users.noreply.github.com>
2023-04-25 05:10:16 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".one").click(function(){
+        alert("아직 미구현 기능 입니다.")
+    });
+
+    $(".ten").click(function(){
+        alert("아직 미구현 기능 입니다.")
+    });
+});
