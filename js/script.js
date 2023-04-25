593588571c83ab554c0efe63cfdc032d1d377d88
hyeonglet <109091684+hyeonglet@users.noreply.github.com>
2023-04-25 10:23:19 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
-    $(".one").click(function(){
-        alert("아직 미구현 기능 입니다.")
-    });
-
     $(".ten").click(function(){
         alert("아직 미구현 기능 입니다.")
     });
+
+    // 단뽑
+    $(".one").click(function(){
+        $(".modal").fadeIn(300);
+    });
+
+    $(".modal > .modal_all > button").click(function(){
+        $(".modal").hide();
+    });
+
+    // const imagesArray = ["니케.png", "대원모집.png", "로비.png", "스쿼드.png", "인벤토리.png"];
+    // const pbtArray = [10, 10, 10, 10, 60];
+    // const randomButton = document.querySelector(".one");
+    // const oneDrawImg = document.querySelector(".draw");
+
+    // function changeImagesHandler(){
+    //     const mainVisual = imagesArray[Math.floor(Math.random() * imagesArray.length)];
+    //     oneDrawImg.src = `images/footer_ui/${mainVisual}`;
+    // };
+
+    // randomButton.addEventListener('click', changeImagesHandler);
+
+
 });
