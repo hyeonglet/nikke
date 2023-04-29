53dfb19a10e535b8766edd566e9a5c648b906865
hyeonglet <109091684+hyeonglet@users.noreply.github.com>
2023-04-30 07:05:26 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -28,20 +28,37 @@ $(document).ready(function(){
 
     // 필그림몰드
     $("#pilgrimBtn").click(function(){
-        $(".modal").fadeIn(300);
+        $(".modal_pilgrim").fadeIn(300);
     });
 
-    // const imagesArray = ["니케.png", "대원모집.png", "로비.png", "스쿼드.png", "인벤토리.png"];
-    // const pbtArray = [10, 10, 10, 10, 60];
-    // const randomButton = document.querySelector(".one");
-    // const oneDrawImg = document.querySelector(".draw");
+    $(".modal_pilgrim > .modal_all_pilgrim > button").click(function(){
+        $(".modal_pilgrim").hide();
+    });
 
-    // function changeImagesHandler(){
-    //     const mainVisual = imagesArray[Math.floor(Math.random() * imagesArray.length)];
-    //     oneDrawImg.src = `images/footer_ui/${mainVisual}`;
-    // };
 
-    // randomButton.addEventListener('click', changeImagesHandler);
+                                // 필그림
+                                pilgrimBtn.addEventListener("click", function() {
+                                    var resultThree = selectItemWithProbability();
+                                    var drawElementThree = document.querySelector(".draw_pilgrim");
+                                    drawElementThree.src = "images/nikke/" + resultThree + ".jpg";
+                                });
+                    
+                                function selectItemWithProbability() {
+                                    var itemsThree = ["iDoll썬", "iDoll오션", "iDoll플라워", "솔져O.W.", "솔져F.A.", "솔져E.G.", "프로덕트23", "프로덕트12", "프로덕트08", "미카", "벨로타", "아니스", "델타", "네온", "라피", "에테르", "미하라", "N102", "모더니아", "라푼젤", "이사벨", "홍련", "노아", "스노우화이트", "하란"];
+                                    var probabilitiesThree = [0.022216, 0.022216, 0.022216, 0.022216, 0.022216, 0.022216, 0.022216, 0.022216, 0.022216, 0.033323, 0.033323, 0.033323, 0.033323, 0.033323, 0.033323, 0.033323, 0.033323, 0.033323, 0.07145, 0.07145, 0.07145, 0.07145, 0.07145, 0.07145, 0.07145];
+                    
+                                    for (var i = 1; i < probabilitiesThree.length; i++) {
+                                        probabilitiesThree[i] += probabilitiesThree[i - 1];
+                                    }
+                                    var randomValueThree = Math.random();
+                    
+                                    for (var i = 0; i < probabilitiesThree.length; i++) {
+                                        if (randomValueThree < probabilitiesThree[i]) {
+                                        return itemsThree[i];
+                                        }
+                                    }
+                                        return itemsThree[itemsThree.length - 1];
+                                }
 
 
 });
